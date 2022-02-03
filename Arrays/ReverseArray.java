aba237af0b92d4998a299551c8627feb01249097
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-03 20:14:48 +0530

    second

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,6 +1,11 @@
 package Arrays;
 public class ReverseArray{
     public static void reverse(int[] array){
+        int l = array.length;
+        for (int i= l-1; i>=0;i--){
+            System.out.println(array[i]+"  ");
+        }
+
 
     }
     public static void main(String[] args) {
