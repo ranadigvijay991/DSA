5b503e708e53a1d39ba70dc6cdbd7b1073337324
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-03 20:26:36 +0530

    final

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,15 +1,31 @@
 package Arrays;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class ReverseArray{
     public static void reverse(int[] array){
         int l = array.length;
         for (int i= l-1; i>=0;i--){
+            System.out.println("Reversed Array : ");
             System.out.println(array[i]+"  ");
         }
 
 
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
+        
+        System.out.println("Enter the size of the array: ");
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] arr = new int[len];
+        
+        System.out.println("Enter the elements of the array : ");
+        for(int i=0; i<len ; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        
         reverse(arr);
         
     }
