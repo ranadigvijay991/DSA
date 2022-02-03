08d3b5e50b9a6d52db86434d7d9fd234c18cb4b3
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-03 20:06:04 +0530

    initial

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+public class ReverseArray{
+    public static void reverse(int[] array){
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        reverse(arr);
+        
+    }
+
+}
