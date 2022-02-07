64aa538995bd6d78bdbb0c3a3e955a6ce592eebd
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-07 20:35:47 +0530

    still need changes

--- Arrays/Union.java
+++ Arrays/Union.java
@@ -2,13 +2,13 @@ public class Union {
     public static void solve(int[] a, int[]b, int n, int m){
         int[] array = new int[m+n];
         for(int i=0;i<n;i++){
-            //int temp = a[i];
-            for(int j=0; j<m+n;j++){
-                if(array[j]!=a[i]){
-                    array[j]=a[i];
-                    break;
-                }
-                
+            for(int j=0;j<m;j++){
+                if(a[i]<b[j]){System.out.println(a[i]);}
+                    
+                else if(a[i]>b[j]){System.out.println(a[i]);}
+                    
+                else if(a[i]==b[j]){System.out.println(b[j]);}
+                    
             }
         }
         for( int i : array){
