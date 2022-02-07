ace17e913ce53c4423668f6108ac6e1852a56b28
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-07 21:00:15 +0530

    final

---
+++ Arrays/CyclicRotation.java
@@ -0,0 +1,20 @@
+public class CyclicRotation 
+{
+
+    public static void rotate(int arr[], int n)
+    {
+        int[] array = new int[n+1];
+        for(int i=0;i<n;i++){
+            array[i+1] = arr[i];
+        }
+        array[0] = arr[n-1];
+        for(int i=0; i<array.length-1;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int n = arr.length;
+        rotate(arr,n);
+    }
+}
