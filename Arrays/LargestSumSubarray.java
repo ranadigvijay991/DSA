bdc6fc3c3ab16d3ec69317ee5d886a8f7ac56dd7
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-07 21:28:35 +0530

    final

---
+++ Arrays/LargestSumSubarray.java
@@ -0,0 +1,23 @@
+public class LargestSumSubarray {
+    public static void solve(int[] arr){
+        int min = Integer.MIN_VALUE;
+        int s = 0;
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            s = s+arr[i];
+            if(min<s){
+                min = s;
+            }
+            if(s<0){
+                s = 0;
+            }
+        }
+        System.out.println(s);
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,-2,5};
+        solve(arr);
+
+    }
+}
