46c162810a8595e42937430657d2173caf9ce559
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-08 20:24:28 +0530

    second

--- Arrays/SortThreeNums.java
+++ Arrays/SortThreeNums.java
@@ -1,14 +1,19 @@
 public class SortThreeNums {
     public static void sort012(int a[], int n)
     {
+    int num = 0;
        for(int i=0;i<3;i++){
-           int num = 0;
+           
            for(int j=0;j<n;j++){
                if (a[j]==num){
                    System.out.print(a[j]+" ");
                }
-            num++;
+            num=num+1;
            }
        }
 }
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 2, 0};
+        sort012(arr, arr.length);
+    }
 }
