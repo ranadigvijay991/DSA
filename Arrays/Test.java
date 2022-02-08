82b463d26719cbd276ffa14bf661566cad8f161a
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-08 20:45:32 +0530

    Test File-

--- Arrays/Test.java
+++ Arrays/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        //
+        //Doesnt work like python
+        System.out.println(arr[-1]);
     }
 }
