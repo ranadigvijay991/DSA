95fea756231571c77320891196c2529e4824702a
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-10 21:00:08 +0530

    final

---
+++ Arrays/MaxProductSubarray.java
@@ -0,0 +1,23 @@
+public class MaxProductSubarray {
+    public static int solution(int[] arr) {
+        int n = arr.length;
+        int max = arr[0];
+        for(int i=0;i<n;i++){
+            int current = arr[i];
+            for(int j=i+1;j<n;j++){
+                max = Math.max(max, current);
+                current = current*arr[j];
+            }
+            max = Math.max(max, current);
+
+
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, -2, -3, 0, 7, -8, -2 };
+        System.out.println(solution(arr));  
+    }
+
+    
+}
