e4501365198112cde2ff75cbffa68362d43544d2
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-15 21:02:41 +0530

    final

--- Arrays/Stock.java
+++ Arrays/Stock.java
@@ -1,6 +1,18 @@
 public class Stock {
     public static void maxProfit(int[] prices, int n){
-        
+        //int n = prices.length;
+        int min = prices[0];
+        int max_profit = 0;
+        for(int i=0;i<n;i++){
+            if(prices[i]<min){
+                min = prices[i];
+                
+            }
+           max_profit = Math.max(max_profit,prices[i] - min);
+            
+           
+        }
+        System.out.println("MAXIMUM PROFIT : "+max_profit);
     }
     public static void main(String[] args) {
         int arr[] = {7,1,5,3,6,4};
