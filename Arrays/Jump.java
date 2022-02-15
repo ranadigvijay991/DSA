b0bc4ed3d000644ef897fc291f21165f3715e975
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-15 23:47:01 +0530

    -

---
+++ Arrays/Jump.java
@@ -0,0 +1,12 @@
+public class Jump {
+    public static void calculate(int[] arr, int n){
+        int target = n-1;
+        int head = arr[0];
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        int n = arr.length;
+        calculate(arr,n);
+    }
+}
