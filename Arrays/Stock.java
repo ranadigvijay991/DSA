67843228198c8c5ec21c91055b83da63da427965
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-15 21:02:17 +0530

    initial

---
+++ Arrays/Stock.java
@@ -0,0 +1,10 @@
+public class Stock {
+    public static void maxProfit(int[] prices, int n){
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int n = arr.length;
+        maxProfit(arr,n);
+    }
+}
