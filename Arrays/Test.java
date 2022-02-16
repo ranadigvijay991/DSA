1117cbc15f9e3d4c664e568cd9c3846cc0cb29ac
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-16 23:59:20 +0530

    Final

--- Arrays/Test.java
+++ Arrays/Test.java
@@ -1,6 +1,10 @@
 public class Test {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,7};
+        int a =6;
+        int b=7;
         //System.out.println(Math.max(arr));
+        System.out.println(Math.max(a,b));
+        System.out.println(5%2);
     }
 }
