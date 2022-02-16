4fe6801f4b83e9e401989fbc300282034b0f0261
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-16 23:47:41 +0530

    final

--- Arrays/PairCount.java
+++ Arrays/PairCount.java
@@ -1,10 +1,19 @@
 public class PairCount {
-    public static void find(int[] arr, int n){
-        
+    public static void find(int[] arr, int n,int k){
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if (arr[i]+arr[j]==k){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
     public static void main(String[] args) {
         int arr[] = {1, 5, 7, 1};
+        int k = 6;
         int n = arr.length;
-        find(arr,n);
+        find(arr,n,k);
     }
 }
