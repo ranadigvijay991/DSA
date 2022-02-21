244c966866b706d5f4c754b23f439f27b8e150ca
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-21 15:28:39 +0530

    final

--- Arrays/Factorial.java
+++ Arrays/Factorial.java
@@ -1,8 +1,33 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class Factorial {
+   
     public static void main(String[] args) {
-        
+        long N= 20;
+         long fact = 1;
+         long j=1;
+        while( j <= N ){  
+            fact = fact * j;   
+            j++; //increment i by 1   
+        }     
+        System.out.println(fact);
+        String s =Long.toString(fact);
+        ArrayList<Long> lis = new ArrayList<Long>();
+        for(int i=0;i<s.length();i++){
+            lis.add(fact%10);
+            fact = fact/10;
+        }
+        ArrayList<Long> rev = new ArrayList<Long>();
+        for(int i=lis.size()-1;i>=0;i--){
+            rev.add(lis.get(i));
+        }
+        for(Long i:rev){
+            System.out.print(i+" ");
+        }
     }
-    
 }
+
+    
+
