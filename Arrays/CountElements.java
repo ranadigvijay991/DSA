f23a4bffc2fca8984c38e3f7d0cf51d0ad793594
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-21 14:22:35 +0530

    -final

--- Arrays/CountElements.java
+++ Arrays/CountElements.java
@@ -1,6 +1,9 @@
 package Arrays;
+
+import java.util.*;
+
 class CountElements{
-    public static void solve(int[] nums){
+    public static List<Integer> solve(int[] nums){
         int n = nums.length;
         int[] count = new int[n];
         for(int i=0;i<n;i++){
@@ -14,6 +17,19 @@ class CountElements{
         for(int i: count){
             System.out.println(i);
         }
+        int ap = n/3;
+        Set<Integer> lis = new LinkedHashSet<Integer>();
+        for(int i=0;i<n;i++){
+            if(count[i]>ap){
+                
+                lis.add(nums[i]);
+            }
+        }
+        
+        List<Integer> list= new ArrayList<Integer>(lis);
+        //list.addAll(lis);
+        
+        return list;
     }
     public static void main(String[] args) {
         int[] nums = {1,2,2};
