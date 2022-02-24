f1f247f2e9a64fe0a8f8d3aca561fb048347065d
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-02-24 20:10:46 +0530

    final

--- Arrays/CheckZeroSub.java
+++ Arrays/CheckZeroSub.java
@@ -1,5 +1,25 @@
+package Arrays;
+import java.util.*;
 public class CheckZeroSub {
+    public static boolean check(int[] arr, int  n){
+        Set<Integer> s = new HashSet<Integer>();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum==0 || arr[i]==0 || s.contains(sum)){
+                return true;
+                //s.add(sum);
+            }
+             s.add(sum);
+       
+        }
+         return false;
+    }
     public static void main(String[] args) {
-        
+        int[] arr = {1,-1,3,-1,4};
+        int n = arr.length;
+        System.out.println(check(arr,n));
+
+
     }
 }
