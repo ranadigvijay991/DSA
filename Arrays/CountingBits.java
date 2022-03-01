eebf542cc41dbde17165ed850bcb4930c98ea182
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-03-01 19:50:43 +0530

    v1

---
+++ Arrays/CountingBits.java
@@ -0,0 +1,18 @@
+package Arrays;
+class CountingBits{
+    public static int[] countBits(int n) {
+        int[] arr = new int[n+1];
+        for(int i=0;i<=n;i++){
+            //Integer first = i;
+            String bin = Integer.toBinaryString(i);
+            int count = 0;
+            count = bin.replaceAll("[^1]", "").length();
+            arr[i] = count;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+    int n = 5;
+    int[] arr = countBits(n);
+    }
+}
