bbe5564167804d9ba485ab9c8bf7a46b465e192f
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-03-01 20:32:41 +0530

    v1

---
+++ Arrays/FindDuplicate.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.*;
+
+public class FindDuplicate {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<Integer>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return num;
+            seen.add(num);
+        }
+        return -1;
+    }
+    
+    
+}
