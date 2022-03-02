fab810a00b5bbfea030741d821af593b2161cb29
Digvijay Rana <53491044+ranadigvijay991@users.noreply.github.com>
2022-03-02 20:10:25 +0530

    v1

---
+++ Arrays/IsSubsequence.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int count = 0;
+        if(s.length()==0){
+            return true ;
+        }
+        for(int i=0;i<t.length();i++){
+            if(t.charAt(i)==s.charAt(count)){
+                 count++;
+                if(count==s.length()){
+                    return true;
+        }
+                
+            }
+        }
+        
+        return false;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
